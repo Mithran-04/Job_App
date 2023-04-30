f4d1edfad8cbb1828a866f43e0001286c7b1c559
Mithran-04 <103376467+Mithran-04@users.noreply.github.com>
2023-05-01 01:35:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,13 @@ import 'package:job_app/screens/user_form_screen.dart';
 import 'package:job_app/screens/Applied_Jobs_screen.dart';
 import 'package:job_app/screens/Archived_jobs_screen.dart';
 
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:sem_5_project/screens/auth_screen1.dart';
-// import 'package:sem_5_project/screens/Events_Details.dart';
-// import 'package:sem_5_project/screens/Form_screen.dart';
-// import 'package:sem_5_project/screens/Registered_Events_screen.dart';
-// import 'package:sem_5_project/screens/Attedance_screen.dart';
-// import 'package:sem_5_project/screens/Form_screenAdmin.dart';
-// import 'package:sem_5_project/screens/Registered_screenAdmin.dart';
-// import 'package:sem_5_project/screens/Home_screenAdmin.dart';
-// import 'package:sem_5_project/screens/Message_AdminScreen.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: FirebaseOptions(
-      apiKey: 'AIzaSyCt4KG50zj5RPQLD5m_eRH40pr3_6jxQoQ',
-      appId: '1:723354314063:android:0395fec12e2c7f3318113a',
+      apiKey: '',
+      appId: '',
       messagingSenderId: '',
-      projectId: 'jobapp-1a125'));
+      projectId: ''));
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
   runApp(MyApp());
 }
@@ -52,13 +42,7 @@ class MyApp extends StatelessWidget{
         AdminFormScreen.routeName:(ctx)=> AdminFormScreen(),
         AppliedUserAdminScreen.routeName:(ctx)=>AppliedUserAdminScreen(),
         ArchivedJobsScreen.routeName:(ctx)=>ArchivedJobsScreen(),
-        // RegisteredEvents.routeName:(ctx)=>RegisteredEvents(),
-        // AttendanceScreen.routeName:(ctx)=>AttendanceScreen(),
-        // FormScreen.routeName:(ctx)=> FormScreen(),
-        // RegisteredScreen.routeName:(ctx)=> RegisteredScreen(),
-        // HomeScreen.routeName:(ctx)=> HomeScreen(),
-        // MessageScreen.routeName:(ctx)=>MessageScreen(),
-        // MessageUserScreen.routeName:(ctx)=> MessageUserScreen(),
+        
       },
     );
   }
